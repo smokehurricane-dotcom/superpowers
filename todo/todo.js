c7d3d9ee25963be0b77d112b948eca47739016bf
Daniel <smokehurricane@gmail.com>
2026-06-22 20:14:23 +0200

    new file:   todo/todo.js

---
+++ todo/todo.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const fs = require('node:fs');
+const path = require('node:path');
+
+const DEFAULT_STORE = path.join(__dirname, 'todos.json');
+
+function readStore(storePath) {
+  try {
+    const raw = fs.readFileSync(storePath, 'utf8');
+    return JSON.parse(raw);
+  } catch (err) {
+    if (err.code === 'ENOENT') return [];
+    throw err;
+  }
+}
+
+function writeStore(storePath, todos) {
+  fs.writeFileSync(storePath, JSON.stringify(todos, null, 2), 'utf8');
+}
+
+function add(text, storePath = DEFAULT_STORE) {
+  const todos = readStore(storePath);
+  const id = Math.max(0, ...todos.map(t => t.id)) + 1;
+  const todo = { id, text, done: false };
+  todos.push(todo);
+  writeStore(storePath, todos);
+  console.log(`Added #${id}: ${text}`);
+  return todo;
+}
+
+function list(storePath = DEFAULT_STORE) {
+  const todos = readStore(storePath);
+  if (todos.length === 0) {
+    console.log('No todos yet.');
+  } else {
+    for (const todo of todos) {
+      const status = todo.done ? 'x' : ' ';
+      console.log(`[${todo.id}] [${status}] ${todo.text}`);
+    }
+  }
+  return todos;
+}
+
+function markDone(id, storePath = DEFAULT_STORE) {
+  const todos = readStore(storePath);
+  const todo = todos.find(t => t.id === id);
+  if (!todo) return null;
+  todo.done = true;
+  writeStore(storePath, todos);
+  console.log(`Done #${id}: ${todo.text}`);
+  return todo;
+}
+
+if (require.main === module) {
+  const [,, command, ...args] = process.argv;
+  switch (command) {
+    case 'add':
+      add(args.join(' '));
+      break;
+    case 'list':
+      list();
+      break;
+    case 'done':
+      markDone(Number(args[0]));
+      break;
+    default:
+      process.stderr.write(`Unknown command: ${command}\nUsage: node todo.js add|list|done\n`);
+      process.exit(1);
+  }
+}
+
+module.exports = { add, list, markDone };
